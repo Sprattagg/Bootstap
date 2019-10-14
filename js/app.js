b1cab4f54c87d64ed5a81a62b024aef0e0984dec
unknown <fredric.dubois@medieinstitutet.se>
2019-10-14 14:44:08 +0200

    new changes

---
+++ js/app.js
@@ -0,0 +1,33 @@
+/* var namn; //variabel deklaration
+namn ="Fredric" // tilldelning, initialize
+console.log(namn);
+
+var age = 29;
+var lastName = "Dubois";
+console.log(age);
+
+document.write(age," ", namn, " ", lastName);
+
+ */
+
+/* var lista = [12, 34,44,55, "saker","vad som helst"]
+
+
+
+for (var i = 0; i < lista.length; i++){
+          document.write(lista[i]);
+}
+
+
+var ar = ["ta", "din", "mä"]
+for (var i = 0; i<ar.length; i++){
+    console.log(ar[i])
+} */
+
+var object = {namn:"Fredric", age:29, yrke:"webbutvecklare", med:"Sweden", isEmpolyed:false };
+
+console.log(object.namn, object.yrke)
+
+var elev = {namn:"Fredric", klass:"wie19", status:"elite"};
+
+document.write(elev.namn, " ", elev.klass," ", elev.status);
